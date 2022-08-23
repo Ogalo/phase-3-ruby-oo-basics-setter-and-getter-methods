7d51f0387ff1af7f3653dba95c2366e33ef54eed
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-08-23 17:42:46 +0300

    commit tree 02e0822a31d418ea0fb47bd7be75f8432dfa86b9

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
+  attr_reader :name
+  attr_accessor :name, :breed
+  #setter
+  def name=(name)
+    @name = name
+  end
+
+  #getter
+  def breed=(breed)
+    @breed = breed
+  end
+
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
+  attr_reader :name
+  attr_accessor :name, :job
+  # Setter method
+  def name=(name)
+    @name = name
+  end
+
+  #getter method
+  def job=(job)
+    @job = job
+  end
 
 end
+
+
+
+
